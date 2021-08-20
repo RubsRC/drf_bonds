dcd7ff2a40cbea8e531ee9922a7d97e89c719388
Ruben Robles <rubsrc@gmail.com>
2021-08-20 11:50:10 -0600

    hello world

--- bonds_project/bonds_site/urls.py
+++ bonds_project/bonds_site/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bonds_app.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world', hello_world, name='hello-world')
 ]
