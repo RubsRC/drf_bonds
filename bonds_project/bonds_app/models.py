2b2e76eb98df9bee9f6fef6036241d0500a40c2f
Ruben Robles <rubsrc@gmail.com>
2021-08-20 12:27:18 -0600

    modelo basico de bono

--- bonds_project/bonds_app/models.py
+++ bonds_project/bonds_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Bond(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=9, decimal_places=4)
+    total = models.IntegerField()
+
+    def __str__(self):
+        return self.name
