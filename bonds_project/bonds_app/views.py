980a4273626265a55be4f6a60552ef7a47057056
Ruben Robles <rubsrc@gmail.com>
2021-08-21 12:44:34 -0600

    comments cleaned

--- bonds_project/bonds_app/views.py
+++ bonds_project/bonds_app/views.py
@@ -17,18 +17,8 @@ def hello_world(request):
     return JsonResponse(data)
 
 
-# class BondListView(generics.ListAPIView):
-#     serializer_class = BondSerializer
-#     queryset = Bond.objects.all()
-
-
-# class BondRetrieveView(generics.RetrieveAPIView):
-#     serializer_class = BondSerializer
-#     queryset = Bond.objects.all()
-
-
 class BondListCreateView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, )
+    # permission_classes = (IsAuthenticated, )
 
     serializer_class = BondSerializer
     queryset = Bond.objects.all()
--- bonds_project/bonds_site/urls.py
+++ bonds_project/bonds_site/urls.py
@@ -1,26 +1,8 @@
-"""bonds_site URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.authtoken.views import obtain_auth_token
 from bonds_app.views import (hello_world,
-                             # BondListView,
-                             # BondRetrieveView,
                              BondListCreateView,
                              BondRetrieveUpdateView)
 
@@ -30,8 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', obtain_auth_token, name='obtain-token'),
     path('hello-world', hello_world, name='hello-world'),
-    # path('bond-list/', BondListView.as_view(), name='bond-list'),
-    # path('bond/<int:pk>', BondRetrieveView.as_view(), name='bond-retrieve'),
     path('api/bond/list-create/', BondListCreateView.as_view(), name='bond-list-create'),
     path('api/bond/<int:pk>/retrieve-update/', BondRetrieveUpdateView.as_view(),
          name='bond-retrieve-update')
