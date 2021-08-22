3441697a546945b2543e40191efa9db00e5a4f60
Ruben Robles <rubsrc@gmail.com>
2021-08-22 15:08:45 -0600

    custom validation on serializer

--- bonds_project/bonds_app/models.py
+++ bonds_project/bonds_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Bond(models.Model):
     name = models.CharField(max_length=40)
-    price = models.DecimalField(max_digits=9, decimal_places=4)
+    price = models.DecimalField(max_digits=13, decimal_places=4)
     total = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- bonds_project/bonds_app/serializers.py
+++ bonds_project/bonds_app/serializers.py
@@ -18,3 +18,27 @@ class BondSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bond
         fields = ('id', 'name', 'price', 'total', 'owner')
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "Ensure this field has more than 3 characters")
+        return value
+
+    def validate_total(self, value):
+        if value < 1:
+            raise serializers.ValidationError(
+                "Ensure this field is bigger than 0")
+        if value > 10000:
+            raise serializers.ValidationError(
+                "Ensure this field is lower than 10,000")
+        return value
+
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                "Ensure this field is 0 or bigger")
+        if value > 100000000.0000:
+            raise serializers.ValidationError(
+                "Ensure this field is lower than 100,000,000.0000")
+        return value
